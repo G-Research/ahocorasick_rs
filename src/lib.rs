fcd479410c04d864a4660a36e3c1904685092eb9
Itamar Turner-Trauring <itamar@itamarst.org>
2021-04-16 14:43:41 -0400

    Just enough wrapping to prove it works.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
+use aho_corasick::AhoCorasick;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+/// A Python wrapper for AhoCorasick.
+#[pyclass(name = "AhoCorasick")]
+struct PyAhoCorasick {
+    ac_impl: AhoCorasick,
+}
+
+/// Methods for PyAhoCorasick.
+#[pymethods]
+impl PyAhoCorasick {
+    /// __new__() implementation.
+    #[new]
+    fn new(patterns: Vec<&str>) -> Self {
+        Self {
+            ac_impl: AhoCorasick::new(patterns),
+        }
+    }
+
+    /// Return matches as indexes into original list of strings.
+    fn find_matches_as_indexes(&self, haystack: &str) -> Vec<usize> {
+        self.ac_impl
+            .find_iter(haystack)
+            .map(|m| m.pattern())
+            .collect()
+    }
 }
 
 /// The main Python module.
 #[pymodule]
 fn ahocorasick_rs(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyAhoCorasick>()?;
     Ok(())
 }
