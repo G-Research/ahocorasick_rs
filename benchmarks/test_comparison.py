5f8931d4718abc992cfe610ef0825ca77c26525e
Itamar Turner-Trauring <itamar@itamarst.org>
2021-04-29 11:37:03 -0400

    A different benchmark. Really we probably want both.

--- benchmarks/test_comparison.py
+++ benchmarks/test_comparison.py
@@ -7,24 +7,30 @@ same haystack would understate ahocorasick_rs's overhead.
 """
 
 import random
+import os
 
 import ahocorasick
 import ahocorasick_rs
 
-PATTERNS = [
-    "abc",
-    "hello",
-    "world",
-    "aardvark",
-    "fish",
-    "what",
-    "arbitrarymonkey",
-    "birds",
-]
-PATTERNS += ["host%d" % i for i in range(500)]
-PATTERNS += [str(random.random()) for i in range(500)]
+# ~5000 popular first names, filtered down to ~4200:
+with open(os.path.join(os.path.dirname(__file__), "names.txt")) as f:
+    PATTERNS = [line.strip().lower() for line in f if len(line.strip()) > 4]
 
-HAYSTACK = "arbitrarymonkey says hello to fish host76, 0.123 my friend, but why???"
+
+# 90% no matches, 10% with 1 match, about 70 characters
+def make_haystacks():
+    line = "No one who had ever seen {} in her infancy would have supposed her born to be an heroine. Her situation in life, the character of her father and mother, her own person and disposition, were all equally against her. Her father was a clergyman, without being neglected, or poor, and a very respectable man, though his name was whatevs—and he had never been handsome. He had a considerable independence besides two good livings—and he was not in the least addicted to locking up his daughters. Her mother was a woman of useful plain sense, with a good temper, and, what is more remarkable, with a good constitution.".lower()
+    result = []
+    for i in range(100000):
+        if i % 90 == 0:
+            name = PATTERNS[i % len(PATTERNS)]
+        else:
+            name = "notaperson"
+        result.append(line.format(name))
+    return result
+
+
+HAYSTACKS = make_haystacks()
 
 
 def make_pyahocorasick_automaton():
@@ -40,8 +46,8 @@ def test_pyahocorasick_overlapping(benchmark):
     automaton = make_pyahocorasick_automaton()
 
     def run():
-        for i in range(10000):
-            x = list(automaton.iter(HAYSTACK + str(i)))
+        for haystack in HAYSTACKS:
+            x = list(automaton.iter(haystack))
         return x
 
     print(benchmark(run))
@@ -52,8 +58,8 @@ def test_pyahocorasick_longest_match(benchmark):
     automaton = make_pyahocorasick_automaton()
 
     def run():
-        for i in range(10000):
-            x = list(automaton.iter_long(HAYSTACK + str(i)))
+        for haystack in HAYSTACKS:
+            x = list(automaton.iter_long(haystack))
         return x
 
     print(benchmark(run))
@@ -64,8 +70,8 @@ def test_ahocorasick_rs_standard(benchmark):
     ac = ahocorasick_rs.AhoCorasick(PATTERNS)
 
     def run():
-        for i in range(10000):
-            x = ac.find_matches_as_strings(HAYSTACK + str(i))
+        for haystack in HAYSTACKS:
+            x = ac.find_matches_as_strings(haystack)
         return x
 
     print(benchmark(run))
@@ -76,8 +82,8 @@ def test_ahocorasick_rs_standard_indexes(benchmark):
     ac = ahocorasick_rs.AhoCorasick(PATTERNS)
 
     def run():
-        for i in range(10000):
-            x = ac.find_matches_as_indexes(HAYSTACK + str(i))
+        for haystack in HAYSTACKS:
+            x = ac.find_matches_as_indexes(haystack)
         return x
 
     print(benchmark(run))
@@ -88,8 +94,8 @@ def test_ahocorasick_rs_overlapping(benchmark):
     ac = ahocorasick_rs.AhoCorasick(PATTERNS)
 
     def run():
-        for i in range(10000):
-            x = ac.find_matches_as_strings(HAYSTACK + str(i), overlapping=True)
+        for haystack in HAYSTACKS:
+            x = ac.find_matches_as_strings(haystack, overlapping=True)
         return x
 
     print(benchmark(run))
@@ -102,8 +108,8 @@ def test_ahocorasick_rs_longest_match(benchmark):
     )
 
     def run():
-        for i in range(10000):
-            x = ac.find_matches_as_strings(HAYSTACK + str(i))
+        for haystack in HAYSTACKS:
+            x = ac.find_matches_as_strings(haystack)
         return x
 
     print(benchmark(run))
@@ -114,7 +120,7 @@ def test_overhead(benchmark):
     ac = ahocorasick_rs.AhoCorasick(PATTERNS)
 
     def run():
-        for i in range(10000):
-            _ = HAYSTACK + str(i)
+        for haystack in HAYSTACKS:
+            _ = haystack
 
     print(benchmark(run))
