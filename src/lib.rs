3c97663dfaf68b2fb01b50d869e368a66a514d34
Itamar Turner-Trauring <itamar@pythonspeed.com>
2023-03-28 09:20:57 -0400

    dfa() has been replaced.

--- src/lib.rs
+++ src/lib.rs
@@ -99,7 +99,8 @@ impl PyAhoCorasick {
         });
         Ok(Self {
             ac_impl: AhoCorasickBuilder::new()
-                .dfa(true) // DFA results in faster matches
+                // TODO consider noncontiguous, and/or auto mode; need to benchmark
+                .kind(aho_corasick::AhoCorasickKind::DFA) // DFA results in faster matches
                 .match_kind(matchkind)
                 .build(patterns.chunks(10 * 1024).flat_map(|chunk| {
                     let result = chunk
